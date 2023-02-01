4b935a24b4724994eafe2b13364e0a35bc5ff465
davidfdr <david.fandrei@gustaveroussy.fr>
2023-02-01 01:00:48 +0100

    Added sacCer3 chromosome file and shell script that merges files

---
+++ bin/transcripts_all_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+inputdir=$1
+cd ${1}
+
+paste */abundance.tsv | cut -f 1,2,5,10,15,20,25,30 > transcript_tpms_all_samples.tsv
+ls -1 */abundance.tsv | perl -ne 'chomp $_; if ($_ =~ /(\S+)\/abundance\.tsv/){print "\t$1"}' | perl -ne 'print "target_id\tlength$_\n"' > header.tsv
+cat header.tsv transcript_tpms_all_samples.tsv | grep -v "tpm" > transcript_tpms_all_samples.tsv2
+mv transcript_tpms_all_samples.tsv2 transcript_tpms_all_samples.tsv
+rm -f header.tsv
